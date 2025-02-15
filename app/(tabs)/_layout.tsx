d4eb6eb74f0d835034cf33986b62721d68332918
jwoodburn01 <jwoodburn01@qub.ac.uk>
2025-02-15 22:05:32 +0000

    Edit 2nd tab name

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
@@ -34,10 +34,13 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="workout"
+        name="explore"
         options={{
           title: 'Workout',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => 
+          // <IconSymbol size={28} name="house.fill" color={color} 
+          <Ionicons name="barbell" size={24} color={color}
+          />,
         }}
       />
     </Tabs>
