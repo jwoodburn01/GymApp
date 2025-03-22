95f3be410cb5ea089f85345b764a892f3770b3c7
jwoodburn01 <jwoodburn01@qub.ac.uk>
2025-03-22 22:55:36 +0000

    Adding more navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,61 +1,36 @@
-import { Image, StyleSheet, Platform, Button } from 'react-native';
+import { Image, StyleSheet, Platform, Button } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Link } from "expo-router";
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/ExpoFitnessLogo.png')}
+          source={require("@/assets/images/ExpoFitnessLogo.png")}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Expo Fitness!</ThemedText>
         {/* <HelloWave /> */}
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Enter a workout</ThemedText>
-        <Button
-  // onPress={onPressLearnMore}
-  title="Enter a Workout"
-  color="#242424"
-  accessibilityLabel="Learn more about this purple button"
-/>
-        {/* <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText> */}
+        {/* <ThemedText type="subtitle">Enter a workout</ThemedText> */}
+        <Link href={"/explore"} asChild>
+          <Button
+            title="Workout"
+            color="#242424"
+            // accessibilityLabel="Learn more about this purple button"
+          />
+        </Link>
       </ThemedView>
-      {/* <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView> */}
     </ParallaxScrollView>
   );
 }
@@ -63,7 +38,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   titleContainer: {
     // flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: "center",
     // gap: 50,
   },
   stepContainer: {
@@ -75,6 +50,6 @@ const styles = StyleSheet.create({
     width: 400,
     bottom: -60,
     left: -10,
-    position: 'absolute',
+    position: "absolute",
   },
 });
---
+++ app/(tabs)/workouts.tsx
@@ -0,0 +1,49 @@
+import { StyleSheet, Image, Platform, Button } from 'react-native';
+// import { Select } from "native-base";
+import { Collapsible } from '@/components/Collapsible';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import { useState } from 'react';
+import DropdownComponent from '@/components/Dropdown';
+
+export default function TabTwoScreen() {
+
+  const [service, setService] = useState();
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={
+     <Image source={require('@/assets/images/muscle-man-lifting-weights.jpg')}/>
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Workouts</ThemedText>
+      </ThemedView>
+      <ThemedText>Choose a previous workout or create a new one</ThemedText>
+      <DropdownComponent />
+
+      <Button
+        // onPress={}
+        title="New Item"
+        color="red"
+        accessibilityLabel="Add a new item"
+      />
+    
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
